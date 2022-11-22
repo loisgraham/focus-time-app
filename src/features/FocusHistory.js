4cc78b3ede416b8859415cfc071fbfe0e058e048
Lois Graham <loisgraham12@gmail.com>
2022-11-22 11:48:50 +0000

    yarn lockfile

--- src/features/FocusHistory.js
+++ src/features/FocusHistory.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { colors } from '../utils/colors';
 import { fontSizes, spacing } from '../utils/sizes';
 
-export const FocusHistory = () => {
+export const FocusHistory = ({history}) => {
   if (!history || !history.length) return null;
 
   const renderItem = ({ item }) => <Text style={styles.item}>- {item}</Text>;
